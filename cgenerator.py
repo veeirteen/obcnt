25e8bd1510e91333fbbb9968055f1d1014e8ce9b
veeirteen <113996381+veeirteen@users.noreply.github.com>
2025-04-10 17:18:34 +0000

    modified:   cgenerator.py 	modified:   cviewer.py 	game.py

--- cgenerator.py
+++ cgenerator.py
@@ -1,13 +1,14 @@
 import random
-items = ["#"," "]
+items = ["#"," ","&","@"]
 def generate():
+    lnyh = len(items) - 1
     chunkx =[]
     o = 0
     while not(o == 8):
         chunklinex = []
         i = 0
         while not(i == 8):
-            chunklinex.append(items[random.randint(0,1)])
+            chunklinex.append(items[random.randint(0,lnyh)])
             i = i + 1
         chunkx.append(chunklinex)
         o = o + 1 
--- cviewer.py
+++ cviewer.py
@@ -1,6 +1,8 @@
+import os
 
 def printc(chunk):
     i = 0
+    os.system('cls' if os.name == 'nt' else 'clear')
     while not(i == 7):
         chunklinex = chunk[i]
         print(chunklinex[0],chunklinex[1],chunklinex[2],chunklinex[3],chunklinex[4],chunklinex[5],chunklinex[6],chunklinex[7])
