a149b61ecce910182a4b81280c7dc26a7c28edb2
veeirteen <113996381+veeirteen@users.noreply.github.com>
2025-04-10 06:11:00 +0000

    Pending changes exported from your codespace

---
+++ cviewer.py
@@ -0,0 +1,7 @@
+
+def printc(chunk):
+    i = 0
+    while not(i == 7):
+        chunklinex = chunk[i]
+        print(chunklinex[0],chunklinex[1],chunklinex[2],chunklinex[3],chunklinex[4],chunklinex[5],chunklinex[6],chunklinex[7])
+        i = i + 1
